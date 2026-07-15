f13bef6172a31e0507ac37f99f74267e511fc33d
Kriti Rai <146384735+kritirai-hyd@users.noreply.github.com>
2026-07-15 17:18:16 +0530

    Refactor Day2CounterApp to implement counter functionality

--- Day2counterapp.jsx
+++ Day2counterapp.jsx
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+
 const Day2CounterApp = () => {
-  const [search, setSearch] = useState("");
-  const data = [
-    {
-      name: "Kriti Rai",
-      jobtitle: "Full Stack Developer",
-    },
-    {
-      name: "Punit Rai",
-      jobtitle: "IT Manager",
-    },
-    {
-      name: "Aradhaya Rai",
-      jobtitle: "Content Creater",
-    },
-  ];
-  const filterData = data.filter((d) => d.name.toLowerCase().includes(search.toLowerCase()));
+  const [count, setCount] = useState(0)
   return (
     <>
-      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-    {filterData.map((i, item) => {
-        return (
-       
-            <h1 key={i}>{item.name}</h1>
- 
-        )
-    })}
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count +1)}>Increment</button><br/>
+    <button onClick={() => setCount(0)}>Reset</button><br/>
+    <button onClick={() => setCount(count-1)}>Decrement</button><br/>
     </>
-  );
-};
-export default Day2CounterApp;
+  )
+}
+
+export default Day2CounterApp
