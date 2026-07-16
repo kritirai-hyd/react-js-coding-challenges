e671792efe745540c14ab27dc0d570939305e379
Kriti Rai <146384735+kritirai-hyd@users.noreply.github.com>
2026-07-16 23:46:46 +0530

    Add files via upload

--- Day1searchfilter.jsx
+++ Day1searchfilter.jsx
@@ -1,32 +1,26 @@
-import React, { useState } from "react";
-const Day2CounterApp = () => {
-  const [search, setSearch] = useState("");
-  const data = [
-    {
-      name: "Kriti Rai",
-      jobtitle: "Full Stack Developer",
-    },
-    {
-      name: "Punit Rai",
-      jobtitle: "IT Manager",
-    },
-    {
-      name: "Aradhaya Rai",
-      jobtitle: "Content Creater",
-    },
-  ];
-  const filterData = data.filter((d) => d.name.toLowerCase().includes(search.toLowerCase()));
+import React, { useState } from 'react'
+
+const Day1searchfilter = () => {
+    const dataUser = [
+        "kriti", 
+        "mithi", 
+        "aman", 
+        "punit", 
+        "saroj"
+    ]
+    const [search, setSearch] = useState("");
+    const filterdata = dataUser.filter(
+        (data) => data.toLowerCase().includes(search.toLowerCase())
+ )
   return (
     <>
-      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-    {filterData.map((i, item) => {
-        return (
-       
-            <h1 key={i}>{item.name}</h1>
- 
-        )
-    })}
+    <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} />
+
+     {filterdata.map((a) => {
+        return <h1>{a}</h1>
+    })} 
     </>
-  );
-};
-export default Day2CounterApp;
+  )
+}
+
+export default Day1searchfilter
